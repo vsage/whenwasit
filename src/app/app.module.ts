cf3a1c902ebd5a978d48ad3be5e569cd40a4da4a
vsage <vsage94@gmail.com>
2017-11-20 17:39:39 +0100

    Added best score storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { HttpModule } from '@angular/http';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -20,7 +21,8 @@ import { QuestionPage } from '../pages/question/question.component';
   imports: [
     BrowserModule,
     HttpModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/question/question.component.ts
+++ src/pages/question/question.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { Platform, NavParams, ViewController } from "ionic-angular";
-import { ToastController } from 'ionic-angular';
-import { QuestionService } from './question.service';
+import { ToastController } from "ionic-angular";
+import { Storage } from "@ionic/storage";
+import { QuestionService } from "./question.service";
 
-import * as _ from 'underscore';
+import * as _ from "underscore";
 
 @Component({
   selector: "question",
@@ -11,35 +12,36 @@ import * as _ from 'underscore';
   providers: [QuestionService]
 })
 export class QuestionPage implements OnInit {
-
   answerValue: any;
   hint: number = 0;
   question: any;
   index: number = 0;
   score: number = 0;
+  best: number;
   hidden1 = -1;
   hidden2 = -1;
   questionsArray: any[];
-
+  category: string;
 
   constructor(
     public platform: Platform,
     public params: NavParams,
     public viewCtrl: ViewController,
     public toastCtrl: ToastController,
-    public questionService: QuestionService
-  ) {
-
-  }
+    public questionService: QuestionService,
+    private storage: Storage
+  ) {}
 
   setQuestions(res) {
     this.questionsArray = res;
     this.question = res[this.index];
   }
 
-  ngOnInit(){
-    this.questionService.getQuestionList(this.params.get('category'))
-    .subscribe( (res) => this.setQuestions(res) )
+  ngOnInit() {
+    this.category = this.params.get("category");
+    this.questionService
+      .getQuestionList(this.category)
+      .subscribe(res => this.setQuestions(res));
   }
 
   dismiss() {
@@ -47,47 +49,65 @@ export class QuestionPage implements OnInit {
   }
 
   answerQuestion(answer) {
-
     let realAnswer = this.hint == 0 ? this.answerValue : answer.answer;
-    let filteredAnswer = _.filter(this.question.answers, (ans) => {
+    let filteredAnswer = _.filter(this.question.answers, ans => {
       return ans.answer == realAnswer;
-    })
-    if (filteredAnswer.length>0 && filteredAnswer[0].right=="yes") {
-      this.presentToast("Well played !")
-      switch(this.hint) {
-         case 0: {
-            this.score+=20;
-            break;
-         }
-         case 1: {
-            this.score+=10;
-            break;
-         }
-         case 2: {
-            this.score+=5;
-            break;
-         }
+    });
+    if (filteredAnswer.length > 0 && filteredAnswer[0].right == "yes") {
+      this.presentToast("Well played !");
+      switch (this.hint) {
+        case 0: {
+          this.score += 20;
+          break;
+        }
+        case 1: {
+          this.score += 10;
+          break;
+        }
+        case 2: {
+          this.score += 5;
+          break;
+        }
       }
+    } else {
+      this.presentToast("Oops !");
     }
-    else {
-      this.presentToast("Oops !")
-    }
-    this.answerValue = '';
+    this.answerValue = "";
     this.index += 1;
     this.question = this.questionsArray[this.index];
     this.hint = 0;
     this.hidden1 = -1;
     this.hidden2 = -1;
+
+    if (this.index == 9) {
+      this.storage.get("best_" + this.category).then(
+        val => {
+          if (val) {
+            this.best = Math.max(val, this.score);
+            if (val<this.score) { this.storage.set("best_" + this.category, this.score); }
+          } else {
+            this.storage.set("best_" + this.category, this.score);
+            this.best = this.score;
+          }
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
   }
 
-  giveHint(){
-    if (this.hint == 1){
-      let wrongAnswerIndex = _.findIndex(this.question.answers, (ans) => {
+  giveHint() {
+    if (this.hint == 1) {
+      let wrongAnswerIndex = _.findIndex(this.question.answers, ans => {
         return ans.right == "yes";
-      })
-      let possibleRemoveIndices = _.difference([0,1,2,3], [wrongAnswerIndex])
-      let firstRand = Math.floor((Math.random() * 3));
-      let secondRand = (firstRand + Math.floor((Math.random() * 2)) + 1) % 3;
+      });
+      let possibleRemoveIndices = _.difference(
+        [0, 1, 2, 3],
+        [wrongAnswerIndex]
+      );
+      let firstRand = Math.floor(Math.random() * 3);
+      let secondRand = (firstRand + Math.floor(Math.random() * 2) + 1) % 3;
       this.hidden1 = possibleRemoveIndices[firstRand];
       this.hidden2 = possibleRemoveIndices[secondRand];
       // console.log(this.hidden1, otherRand, this.hidden2)
@@ -103,5 +123,4 @@ export class QuestionPage implements OnInit {
     });
     toast.present();
   }
-
 }
